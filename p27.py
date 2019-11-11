46ee9a83e1d4e3fffbcf173d7f94ad485350160b
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 03:26:04 +0600

    Update p27.py

--- p27.py
+++ p27.py
@@ -1,3 +1,6 @@
+# For the given integer X print 1 if it's positive, -1 if it's negative, or 0 if it's equal
+# to zero.
+# Try to use the cascade if-elif-else for it.
 x=int(input("Enter your number:"))
 if x<0:
 	print(-1)
